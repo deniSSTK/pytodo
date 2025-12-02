a59bf6776c83814deb220bcb4005055b28d1905f
DenisSTK <denisdrawler@gmail.com>
2025-12-02 14:04:20 +0200

    Moved endpoints

--- app/routers/user.py
+++ app/routers/user.py
@@ -7,10 +7,10 @@ from services.user import user_service
 
 router = APIRouter(prefix="/user", tags=["user"])
 
-@router.post("/", response_model=UserRead)
-async def _create_user(user: UserCreate, session: AsyncSession = Depends(get_session)):
-    return await user_service.create_user(session, user)
-
 @router.get("/{user_id}", response_model=UserRead)
 async def _get_user(user_id: str, session: AsyncSession = Depends(get_session)):
     return await user_service.get_user(session, user_id)
+
+@router.post("/", response_model=UserRead)
+async def _create_user(user: UserCreate, session: AsyncSession = Depends(get_session)):
+    return await user_service.create_user(session, user)
